dd2c12424c713a919c4ad9520a67e2a2150a483d
Alexandr Lelikov <lelikovas@gmail.com>
2020-03-31 00:19:48 +0300

    Changed static files settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -120,15 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 LOGIN_URL = '/'
 
 ROLLBAR = {
@@ -138,4 +129,8 @@ ROLLBAR = {
     'root': '/absolute/path/to/code/root',
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
 django_heroku.settings(locals())
