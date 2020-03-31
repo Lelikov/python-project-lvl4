d02562beeb510757348dccc8e1ee192c1685987f
Alexandr Lelikov <lelikovas@gmail.com>
2020-03-31 15:36:03 +0300

    Changed settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -84,7 +84,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {'ENGINE': os.getenv('ENGINE')}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
